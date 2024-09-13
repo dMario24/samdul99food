725cb16f47eb0fb5ce778282b2c08de3d4c20423
dMario24 <data.mario24@gmail.com>
2024-09-13 18:17:46 +0900

    0.5.0

--- src/samdul77food/main.py
+++ src/samdul77food/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pytz
 
 app = FastAPI()
 
@@ -26,4 +27,24 @@ def food(name: str):
     # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
     # DB 저장
     print("==============" + name )
-    return {"food": name, "time": "2024-09-15 11:12:13"}
+
+    from datetime import datetime
+    import pytz
+    timezone = pytz.timezone('Asia/Seoul')
+    korea_time = datetime.now(timezone)
+    formatted_time = korea_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    import pymysql
+    db = pymysql.connect(host = '172.18.0.1',
+                     port = 13306,
+                     user = 'food',
+                     passwd = '1234',
+                     db = 'fooddb',
+                     charset = 'utf8')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    sql = "INSERT INTO foodhistory(username, foodname, dt) VALUES(%s,%s,%s)"
+    cursor.execute(sql, ('n77', name, formatted_time))
+    db.commit()
+
+    return {"food": name, "time": formatted_time}
